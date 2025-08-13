3c7083dc39a340e289d6d49fd29741dc3d6600d6
Akshit Gulyan <103456810+AkshitGulyan@users.noreply.github.com>
2025-08-13 13:55:08 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: { techno: ['ui-sans-serif','system-ui'] }
+    },
+  },
+  plugins: [],
+}
