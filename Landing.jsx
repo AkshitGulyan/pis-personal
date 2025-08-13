4588310eaf8de22d71ce95a66e6fc973ba9c234c
Akshit Gulyan <103456810+AkshitGulyan@users.noreply.github.com>
2025-08-13 13:58:11 +0530

    Create Landing.jsx

---
+++ Landing.jsx
@@ -0,0 +1,6 @@
+import { motion, AnimatePresence } from "framer-motion"
+
+export default function Landing({ onEnter }) {
+  const [launching, setLaunching] = useState(false)
+
+  function handleExplore
